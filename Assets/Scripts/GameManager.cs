21cc70a8155450f2bea26e49625b17060588a3aa
fj123u <theo.girard.1807@gmail.com>
2026-01-29 00:07:51 +0100

    Add ImageButton prefab, GameManager & 2D sprite
    Add an ImageButton prefab (and meta) and a GameManager script that populates a level select panel by instantiating Image prefabs from a list of Texture2D. Add folder meta files for Prefabs and Scripts. Update texture importer settings for Assets/Textures/terre.jpeg to treat it as a sprite (disable mipmaps, enable alpha transparency, set wrap to repeat, non-power-of-two scaling, sprite mode, and assign a sprite ID). Add the com.unity.2d.sprite package to the manifest and update package lock entries.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameManager : MonoBehaviour
+{
+    [Header("UI Elements")]
+    [SerializeField] private List<Texture2D> imageTextures;
+    [SerializeField] private Transform levelSelectPanel;
+    [SerializeField] private Image levelSelectPrefab;
+
+    void Start()
+    {
+        //create the UI
+        foreach (Texture2D texture in imageTextures)
+        {
+            Image image = Instantiate(levelSelectPrefab, levelSelectPanel);
+            image.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
+        }
+    }
+}
