cd1e564354a87d70ba62bb902ead4f036c2082f3
Firefox <firefox42c@gmail.com>
2026-01-28 17:01:57 +0100

    PlayerControllerScript

---
+++ Assets/CharacterController/Scriptes/PlayerController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+
+//PlayerController
+
+public class PlayerController : MonoBehaviour
+{
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
