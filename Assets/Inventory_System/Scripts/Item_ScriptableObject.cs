53e04364ed48b7dca110b4fc1ddcc8aad187d26f
tipatenet <timpaedit@gmail.com>
2026-01-30 02:31:23 +0100

    InventorySystemeStart

---
+++ Assets/Inventory_System/Scripts/Item_ScriptableObject.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Item_ScriptableObject" , menuName = "Item/Item_ScriptableObject")]
+
+public class Item_ScriptableObject : ScriptableObject
+{
+
+}
