7b77016ef4a759c90cc6b24014d584a848330e6d
tipatenet <timpaedit@gmail.com>
2026-01-31 21:49:34 +0100

    Inventory UI

--- Assets/Inventory_System/Scripts/InventorySystem.cs
+++ Assets/Inventory_System/Scripts/InventorySystem.cs
@@ -1,10 +1,20 @@
+using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Progress;
+using static UnityEngine.GraphicsBuffer;
 
 public class InventorySystem : MonoBehaviour
 {
     public Interact interaction;
     public PlayerInputHandler keySystem;
     private Item_ScriptableObject itemPickUp;
+    public float scroolValue = 0f;
+    public int inventorySize = 10;
+    public List<Item_ScriptableObject> inventory = new List<Item_ScriptableObject>(10);
+    public RectTransform item_Container;
+    public int selectedIndex = 0;
+    private float targetX;
+    public float scrollSpeed;
 
     private void Start()
     {
@@ -14,6 +24,8 @@ public class InventorySystem : MonoBehaviour
     private void Update()
     {
         PickUpItem();
+        scroolValue = Input.mouseScrollDelta.y;
+        ScroolInventory();
     }
 
     private void PickUpItem()
@@ -23,5 +35,21 @@ public class InventorySystem : MonoBehaviour
             itemPickUp = interaction.InteractionTrace();
             print(itemPickUp.item_Name);
         }
+    }
+
+    private void ScroolInventory()
+    {
+        if (scroolValue > 0)
+            selectedIndex--;
+        else if (scroolValue < 0 && selectedIndex != 9)
+            selectedIndex++;
+        selectedIndex = Mathf.Clamp(selectedIndex, 0,inventorySize);
+
+        float itemWidth = 100f + 10;
+        targetX = -selectedIndex * itemWidth;
+        Vector2 currentPos = item_Container.anchoredPosition;
+        currentPos.x = Mathf.Lerp(currentPos.x, targetX, Time.deltaTime * scrollSpeed);
+        item_Container.anchoredPosition = currentPos;
+
     }
 }
