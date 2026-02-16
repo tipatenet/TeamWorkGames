024e65d7c510cbf177d2906bcce1c1037da233cd
tipatenet <timpaedit@gmail.com>
2026-02-16 17:07:30 +0100

    Ajout du script opening system pour pouvoir ouvrir tous

---
+++ Assets/Opening_System/Scripts/Opening_System.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using UnityEngine;
+
+public class Opening_System : MonoBehaviour
+{
+    public enum typeOfOpening
+    {
+        Translation,
+        Rotation
+    }
+
+    [Header("Settings")]
+
+    public float timeToOpen = 1f;
+    public GameObject objectHaveToMove;
+    public typeOfOpening typeOfMouvement;
+    public Vector3 amountOpen;
+
+    [Header("States")]
+
+    private bool isMoving = false;
+    private bool isOpen = false;
+
+    //Fonction qui permet de lancer l'ouverture
+    public void StartOpening()
+    {
+        if (!isOpen)
+        {
+            if (!isMoving)
+            {
+                ModeSelected(amountOpen);
+                StartCoroutine(coolDownMoving());
+                isOpen = true;
+            }
+        }
+    }
+
+    public void Close()
+    {
+        if (isOpen)
+        {
+            if (!isMoving)
+            {
+                ModeSelected(-amountOpen);
+                StartCoroutine(coolDownMoving());
+                isOpen = false;
+            }
+        }
+    }
+
+    //Fonction qui fait le mouvement en fonction du mode de mouvement
+    public void ModeSelected(Vector3 amount)
+    {
+        if (typeOfMouvement == typeOfOpening.Rotation)
+        {
+            iTween.RotateAdd(objectHaveToMove, amount, timeToOpen);
+        }
+        else if(typeOfMouvement == typeOfOpening.Translation)
+        {
+            iTween.MoveBy(objectHaveToMove, amount, timeToOpen);
+        }
+    }
+
+    /*CoolDown pour eviter que la fonction soit appelée
+      alors que l'animation n'est pas finis*/
+    IEnumerator coolDownMoving()
+    {
+        isMoving = true;
+        yield return new WaitForSeconds(timeToOpen);
+        isMoving = false;
+    }
+}
